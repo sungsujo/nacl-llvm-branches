959547601c37e966a4f68a4bc872b73532f2ae22
robertm <robertm@wotan.nyc.corp.google.com>
2010-06-11 16:01:57 -0400

    prevent some jumptable to be emitted into .text

--- llvm-trunk/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm-trunk/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -658,6 +658,7 @@ void AsmPrinter::EmitConstantPool() {
 /// by the current function to the current output stream.  
 ///
 void AsmPrinter::EmitJumpTableInfo() {
+  O << "# @LOCALMOD: JUMPTABLE\n"; // @LOCALMOD
   const MachineJumpTableInfo *MJTI = MF->getJumpTableInfo();
   if (MJTI == 0) return;
   if (MJTI->getEntryKind() == MachineJumpTableInfo::EK_Inline) return;
@@ -676,7 +677,15 @@ void AsmPrinter::EmitJumpTableInfo() {
       // in discardable section
       // FIXME: this isn't the right predicate, should be based on the MCSection
       // for the function.
-      F->isWeakForLinker()) {
+      // @LOCALMOD-START
+      // the original code is a hack
+      // jumptables usually end up in .rodata
+      // but for functions with weak linkage there is a chance that the are no needed.
+      // So in order to be discard the function AND the jumptable they keep them both in .text
+      // This fix only works if we never discard weak functions. 
+      // This is guaranteed because the bitcode linker already throws out unused ones.
+      // @LOCALMOD-END
+      false /* F->isWeakForLinker()  */) { // @LOCALMOD
     OutStreamer.SwitchSection(getObjFileLowering().SectionForGlobal(F,Mang,TM));
   } else {
     // Otherwise, drop it in the readonly section.
